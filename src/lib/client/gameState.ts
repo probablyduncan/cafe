d78fd29b6a5d5665a058e42d7a9896423026edaa
dpetrie2 <dpetrie2@origamirisk.com>
2025-05-10 12:28:39 +0100

    comments and save tweaks

--- src/lib/client/gameState.ts
+++ src/lib/client/gameState.ts
@@ -2,19 +2,53 @@ import { getNodeKey, nodePositionsToPath, parseNodeKey, pathToNodePositions, toN
 import { SAVE_DATA_KEY, type NodePosition, type RenderableChoice, type StateCondition } from "../contentSchemaTypes";
 import type { ISaveStore } from "./saveStore";
 
-type SaveJSON = {
-    // this stuff only gets updated on screen clear
-    s?: string;    // scene path, "scene1:c3/scene2:node7"
-    c?: string;     // made choices, "scene1:c1,scene1:c3"
-    k?: string;     // true state keys, "name,name2,name3"
+/**
+ * Save data that gets updated on screen clear. Used as a starting point for reloading save.
+ */
+type SaveState_UpdatedOnClear = {
 
-    l?: string;    // scene:node, last clear choice. If undefined, screen hasn't been cleared yet
+    /**
+     * Path from base to current nested scene, like `"scene1:c3/scene2:node7"`.
+     * If undefined, we're not in a nested scene, and ending this scene will end the game.
+     */
+    s?: string;
+    
+    /**
+     * All visited choices, like `"scene1:c1,scene1:c3"`.
+     * If undefined, no chocies have been made.
+     */
+    c?: string;
 
-    // all choices since last screen clear. Includes last screen clear and most recent choice
-    h?: string;    // "scene1:c3/scene2:node7"
+    /**
+     * All true state keys, like `"name,name2,name3"`.
+     * If undefined, no keys are true.
+     */
+    k?: string;
+
+    /**
+     * Last choice that cleared the screen, like `"scene7:node3"`.
+     * If undefined, screen hasn't been cleared yet. 
+     */
+    l?: string;
+};
+
+/**
+ * Save data that gets updated on every choice. Used to retrace choices from the last clear.
+ */
+type SaveState_UpdatedOnChoose = {
+    
+    /**
+     * All choices made since (not including) the last screen clear, like `"scene1:c3/scene2:node7"`.
+     */
+    h?: string;
 }
 
+type SaveState = SaveState_UpdatedOnClear & SaveState_UpdatedOnChoose;
+
 export type StateOptions = {
+    /**
+     * If true, choices/state/etc will be saved every time a choice is made.
+     */
     autosave: boolean;
 }
 
@@ -46,15 +80,15 @@ export interface IGameState {
     };
 
     /**
+     * Can be used to manually save the game.
+     * Useful if autosave is off.
+     */
+    save(): void;
+
+    /**
      * Returns true if the given choice has already been visited.
      */
     wasChoiceMade(choice: Pick<RenderableChoice, "nodeId" | "sceneId">): boolean;
-
-    /**
-     * Updates state keys with the given condition if it exists,
-     * and returns true if the condition was changed.
-     */
-    // setCondition(condition: StateCondition | undefined): boolean;
 
     /**
      * Returns true if the condition is met or undefined,
@@ -64,23 +98,58 @@ export interface IGameState {
 }
 
 export class GameState implements IGameState {
+    /**
+     * All choices made, as node keys like `"scene2:node4"`.
+     */
+    private _choices: Set<string> = new Set();
 
-    // these props are up to date, but will only get saved on screen clear
-    private _choices: Set<string> = new Set();  // stringified node positions
-    private _keys: Set<string> = new Set();     
-    private _scenePath: NodePosition[] = [];    // will NOT include the current scene.
+    /**
+     * All state keys currently true.
+     */
+    private _keys: Set<string> = new Set(); 
+    
+    /**
+     * Nested scene path used to traverse back to parent scenes.
+     * The last element is the previous scene.
+     * NodeId is the node that entered the current scene.
+     */
+    private _scenePath: NodePosition[] = [];
 
+    /**
+     * The most recent choice that cleared the screen.
+     */
     private _lastClearChoice: NodePosition | undefined;
-    private _lastClearState: Omit<SaveJSON, "h" | "l"> = {}
 
-    // [0] is last clear choice, last el is most recent choice
+    /**
+     * LastClearStateJSON containing save data as of the last time the screen was cleared.
+     */
+    private _lastClearState: SaveState_UpdatedOnClear = {}
+
+    /**
+     * All choices made since (not including) the choice that last cleared the screen.
+     */
     private _choicePath: NodePosition[] = [];
 
-    private _autosave: boolean = false;
+    private _options: StateOptions;
+    private static DEFAULT_OPTIONS: StateOptions = {
+        autosave: true,
+    }
 
     constructor(private readonly _saveStore: ISaveStore, options?: Partial<StateOptions>) {
-        if (options?.autosave !== undefined) {
-            this._autosave = options.autosave;
+        this._setOptions(options);
+    }
+
+    private _setOptions(options?: Partial<StateOptions>) {
+        this._options = { ...GameState.DEFAULT_OPTIONS };
+
+        if (options === undefined) {
+            return;
+        }
+
+        for (const [key, value] of Object.entries(options)) {
+            if (value !== undefined && value !== null) {
+                this._options[key] = value;
+            }
         }
     }
 
@@ -104,8 +173,8 @@ export class GameState implements IGameState {
             this._addChoiceToPath(choice);
         }
 
-        if (this._autosave) {
-            this._save();
+        if (this._options.autosave) {
+            this.save();
         }
     }
 
@@ -116,7 +185,9 @@ export class GameState implements IGameState {
     private _updateLastClearState(choice: NodePosition) {
         this._choicePath = [];
         this._lastClearChoice = toNodePosition(choice);
+
         this._lastClearState = {};
+        this._lastClearState.l = getNodeKey(choice);
 
         const choices = [...this._choices];
         if (choices.length > 0) {
@@ -133,19 +204,15 @@ export class GameState implements IGameState {
         }
     }
 
-    private _save() {
+    save() {
 
-        const saveData: SaveJSON = { ...this._lastClearState };
-
-        if (this._lastClearChoice !== undefined) {
-            saveData.l = getNodeKey(this._lastClearChoice);
-        }
+        const saveData: SaveState = { ...this._lastClearState };
 
         if (this._choicePath.length > 0) {
             saveData.h = nodePositionsToPath(this._choicePath);
         }
 
-        this._saveStore.set<SaveJSON>(SAVE_DATA_KEY, saveData);
+        this._saveStore.set<SaveState>(SAVE_DATA_KEY, saveData);
     }
 
     loadToLastClear(): {
@@ -153,7 +220,7 @@ export class GameState implements IGameState {
         choicePath: NodePosition[];
     } {
 
-        const saveData = this._saveStore.get<SaveJSON>(SAVE_DATA_KEY);
+        const saveData = this._saveStore.get<SaveState>(SAVE_DATA_KEY);
         if (saveData === undefined) {
             return {
                 lastClear: undefined,
